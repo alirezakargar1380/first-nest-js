0500eb9140ef060bd78ecafbf37034024ffb62c9
alireza kargar <imanesche@gmail.com>
2022-06-12 03:44:22 +0430

    added item controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestController } from './test/test.controller';
+import { ItemsController } from './items/items.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, TestController],
+  controllers: [AppController, TestController, ItemsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 
 @Controller('test/t')
 export class TestController {
-    @Get()
-    getHello(): string {
-        return 'Hello Test!';
+    @Get('/t')
+    getUrlLink(@Request() req: Request): string {
+        return req.url;
     }
 }
